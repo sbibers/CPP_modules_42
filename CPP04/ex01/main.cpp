b9ec3b8aaac2167d25ff30b7b5c0f16eb76857c3
salam <salambaybars@gmail.com>
2025-03-10 18:22:01 +0300

    CPP04 ex01 remove comment

--- CPP04/ex01/main.cpp
+++ CPP04/ex01/main.cpp
@@ -19,39 +19,6 @@
 
 int main()
 {
-    // std :: cout << "------------Correct Animal Tests------------" << std :: endl;
-    // const Animal    *meta = new Animal("Animal");
-    // const Animal    *i = new Dog();
-    // const Animal    *j = new Cat();
-    
-    // std :: cout << "Dog type: " << i->getType() << std :: endl;
-    // std :: cout << "Cat type: " << j->getType() << std :: endl;
-    // std :: cout << "Dog sound: ";
-    // i->makeSound();
-    // std :: cout << "Cat sound: ";
-    // j->makeSound();
-    // std :: cout << "Animal sound: ";
-    // meta->makeSound();
-    // delete (i);
-    // delete (j);
-    // delete (meta);
-    
-    // std :: cout << "------------Wrong Animal Tests------------" << std :: endl;
-    // const WrongAnimal   *wrong_animal = new WrongAnimal();
-    // const WrongAnimal   *wrong_cat = new WrongCat();
-    // const WrongCat      *direct_wrong_cat = new WrongCat();
-    
-    // std :: cout << "WrongCat type: " << wrong_cat->getType() << std :: endl;
-    // std :: cout << "WrongCat through WrongAnimal pointer sound: ";
-    // wrong_cat->makeSound(); // call WrongAnimal::makeSound(), because it does not have virtual function
-    // std :: cout << "WrongAnimal sound: ";
-    // wrong_animal->makeSound();  // call WrongAnimal::makeSound(), because it does not have virtual function
-    // std :: cout << "Direct WrongCat sound: ";
-    // direct_wrong_cat->makeSound(); // call WrongCat::makeSound()
-    // delete (wrong_animal);
-    // delete (wrong_cat);
-    // delete (direct_wrong_cat);
-
     std :: cout << "------------Test Animal Object Array------------" << std :: endl;
     Animal *animals[10];
     for (int i = 0; i < 10; i++)
