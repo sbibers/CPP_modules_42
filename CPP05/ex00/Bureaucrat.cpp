590cd5b7a603039f9173acb0a7f511e65327b51e
Sallam Bibers <sbibers@c1r2s13.42amman.com>
2025-05-12 13:01:33 +0300

    CPP05/ex00 done 100/100

--- CPP05/ex00/Bureaucrat.cpp
+++ CPP05/ex00/Bureaucrat.cpp
@@ -3,48 +3,57 @@
 /*                                                        :::      ::::::::   */
 /*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbibers <sbibers@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sbibers <sbibers@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 13:13:49 by sbibers           #+#    #+#             */
-/*   Updated: 2025/05/11 17:18:36 by sbibers          ###   ########.fr       */
+/*   Updated: 2025/05/12 12:53:24 by sbibers          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
+#include "Bureaucrat.hpp"
+
 const char *Bureaucrat::GradeTooHighException::what() const throw()
 {
-    return ("Exception : Grade too high!");
+    return ("Bureaucrat, Grade Too High!");
 }
 
 const char *Bureaucrat::GradeTooLowException::what() const throw()
 {
-    return ("Exception : Grade too low!");
+    return ("Bureaucrat, Grade Too Low!");
 }
 
-Bureaucrat::Bureaucrat() : name("haha"), grade(27)
+Bureaucrat::Bureaucrat() : name("default name"), grade(27)
 {
-    std::cout << "Default constructor called, default grade = 27\n";
+    std::cout << "Bureaucrat default caonstructor called, name = default name, grade = 27\n";
 }
 
-Bureaucrat::Bureaucrat(const std::string &name, const int grade) : name(name)
+Bureaucrat::Bureaucrat(std::string name, int grade) : name(name)
 {
     if (grade < 1)
-        throw Bureaucrat::GradeTooHighException();
+        throw Bureaucrat::GradeTooHighException(); // throw will stop the try block and put the message of exception on a catch in main function.
     else if (grade > 150)
         throw Bureaucrat::GradeTooLowException();
-    else
-        this->grade = grade;
-}
-
-Bureaucrat::~Bureaucrat()
-{
-    std::cout << "Destructor called\n";
+    this->grade = grade;
+    std::cout << "Bureaucrat paramterize constructor called\n";
 }
 
 Bureaucrat::Bureaucrat(const Bureaucrat &copy) : name(copy.name), grade(copy.grade)
 {
-    std::cout << "Copy constructor called\n";
+    std::cout << "Bureaucrat copy constructor called\n";
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &copy)
+{
+    std::cout << "Bureaucrat copy assignment operator called\n";
+    this->grade = copy.grade;
+    return (*this);
+}
+
+Bureaucrat::~Bureaucrat()
+{
+    std::cout << "Bureaucrat destructor called\n";
 }
 
 const std::string &Bureaucrat::getName() const
@@ -57,28 +66,21 @@ int Bureaucrat::getGrade() const
     return (this->grade);
 }
 
-Bureaucrat &Bureaucrat::operator=(const Bureaucrat &copy)
-{
-    std::cout << "Copy assignment operator called\n";
-    this->grade = copy.getGrade();
-    return (*this);
-}
-
 void Bureaucrat::increment_grade()
 {
     if ((this->grade - 1) < 1)
-        throw Bureaucrat::GradeTooLowException();
+        throw Bureaucrat::GradeTooHighException();
     this->grade--;
 }
 
 void Bureaucrat::decrement_grade()
 {
     if ((this->grade + 1) > 150)
-        throw Bureaucrat::GradeTooHighException();
+        throw Bureaucrat::GradeTooLowException();
     this->grade++;
 }
 
-std::ostream& operator<<(std::ostream& out, const Bureaucrat& obj)
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &obj)
 {
     out << obj.getName() << ", Bureaucrat Grade : " << obj.getGrade();
     return (out);
--- CPP05/ex00/main.cpp
+++ CPP05/ex00/main.cpp
@@ -3,35 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbibers <sbibers@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sbibers <sbibers@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 12:31:50 by sbibers           #+#    #+#             */
-/*   Updated: 2025/05/11 17:18:13 by sbibers          ###   ########.fr       */
+/*   Updated: 2025/05/12 13:01:08 by sbibers          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
-#include <limits>
 
 int main()
 {
-    // try
-    // {
-    //     Bureaucrat a("salam", 0);
-    // }
-    // catch (const std::exception &e)
-    // {
-    //     std::cerr << e.what() << '\n';
-    // }
     try
     {
-        Bureaucrat b("salam", 150);
-        b.decrement_grade();
-        std::cout << b << std::endl;
+        Bureaucrat a("salam", 0);
     }
     catch (const std::exception &e)
     {
         std::cerr << e.what() << std::endl;
+    }
+    // std::exit(0);
+    std::cout << "=========================================\n";
+    Bureaucrat *a = new Bureaucrat("salam", 150);
+    try
+    {
+        a->decrement_grade();
+        std::cout << *a << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    delete a;
+    // std::exit(0);
+    std::cout << "=========================================\n";
+    try
+    {
+        Bureaucrat *b = new Bureaucrat("Boss", 28);
+        if (b != NULL)
+        {
+            std::cout << *b << std::endl;
+            delete b;
+        }
+        else
+            std::cout << "Pointer NULL\n";
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
     }
     return (0);
 }
