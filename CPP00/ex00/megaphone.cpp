403858163741a6bab485f8f5c5dc0eeec0cd23c2
Sallam Bibers <sbibers@c1r5s7.42amman.com>
2025-08-04 19:25:20 +0300

    Edit CPP00/ex00 :)

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: sbibers <sbibers@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 21:59:45 by salam             #+#    #+#             */
-/*   Updated: 2025/02/23 17:35:18 by sbibers          ###   ########.fr       */
+/*   Updated: 2025/08/04 19:24:51 by sbibers          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,14 @@ int main(int argc, char *argv[])
         std :: cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std :: endl;
         return (1);
     }
-    argv = &argv[1];
-    for (size_t i = 0; argv[i]; i++)
+    for (size_t i = 1; argv[i]; i++)
     {
+        std::string str;
         for (size_t j = 0; argv[i][j]; j++)
         {
-            std :: cout << (char)toupper(argv[i][j]);
+            str += toupper(argv[i][j]);
         }
+        std::cout << str;
     }
     std :: cout << std :: endl;
     return (0);
